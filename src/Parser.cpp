fd8817d16c17f6c4352f8d5af816cb31f597940e
vfegger <vitor@psr-inc.com>
2023-03-09 16:32:31 -0300

    Minor Fix

--- src/Parser.cpp
+++ src/Parser.cpp
@@ -1,6 +1,17 @@
 #include "../include/Parser.hpp"
 
-Parser::Parser(unsigned length_in){
+Parser::Parser()
+{
+    length_In = 0u;
+    length_Out = 0u;
+    fileArray_In = NULL;
+    fileArray_Out = NULL;
+    count_In = 0u;
+    count_Out = 0u;
+}
+
+Parser::Parser(unsigned length_in)
+{
     length_In = length_in;
     length_Out = length_in;
     fileArray_In = new std::ifstream[length_in];
@@ -9,12 +20,14 @@ Parser::Parser(unsigned length_in){
     count_Out = 0u;
 }
 
-void Parser::ImportConfiguration(std::ifstream& file_in, std::string& name_out, unsigned& length_out, ParserType& type_out, unsigned& iteration_out){
+void Parser::ImportConfiguration(std::ifstream &file_in, std::string &name_out, unsigned &length_out, ParserType &type_out, unsigned &iteration_out)
+{
     char aux[100];
     unsigned type;
-    file_in.getline(aux,100);
+    file_in.getline(aux, 100);
     name_out = aux;
-    if(name_out.size() > 0u && name_out[name_out.size()-1u] == '\r'){
+    if (name_out.size() > 0u && name_out[name_out.size() - 1u] == '\r')
+    {
         name_out.pop_back();
     }
     file_in >> length_out;
@@ -23,7 +36,8 @@ void Parser::ImportConfiguration(std::ifstream& file_in, std::string& name_out, 
     type_out = (ParserType)type;
 }
 
-void Parser::ExportConfiguration(std::ofstream& file_in, std::string name_in, unsigned length_in, ParserType type_in, std::streampos& iterationPosition_out){
+void Parser::ExportConfiguration(std::ofstream &file_in, std::string name_in, unsigned length_in, ParserType type_in, std::streampos &iterationPosition_out)
+{
     std::string iteration_string = std::to_string(0u) + "    ";
     file_in << name_in << "\n";
     file_in << length_in << "\t";
@@ -32,10 +46,12 @@ void Parser::ExportConfiguration(std::ofstream& file_in, std::string name_in, un
     file_in << iteration_string << "\n";
 }
 
-void Parser::ImportValues(std::ifstream& file_in, unsigned length_in, ParserType type_in, void*& values_out, unsigned iteration_in){
+void Parser::ImportValues(std::ifstream &file_in, unsigned length_in, ParserType type_in, void *&values_out, unsigned iteration_in)
+{
     std::streampos position = file_in.tellg();
-    for(unsigned i = 0u; i < iteration_in * length_in; i++){
-        file_in.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
+    for (unsigned i = 0u; i < iteration_in * length_in; i++)
+    {
+        file_in.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
     }
     switch (type_in)
     {
@@ -97,7 +113,8 @@ void Parser::ImportValues(std::ifstream& file_in, unsigned length_in, ParserType
     file_in.seekg(position);
 }
 
-void Parser::ExportValues(std::ofstream& file_in, unsigned length_in, ParserType type_in, void* values_in, std::streampos& iterationPosition_in, unsigned iteration_in){
+void Parser::ExportValues(std::ofstream &file_in, unsigned length_in, ParserType type_in, void *values_in, std::streampos &iterationPosition_in, unsigned iteration_in)
+{
     std::streampos position = file_in.tellp();
     file_in.seekp(iterationPosition_in);
     file_in << iteration_in + 1u;
@@ -148,7 +165,8 @@ void Parser::ExportValues(std::ofstream& file_in, unsigned length_in, ParserType
     }
 }
 
-void Parser::ImportAllValues(std::ifstream& file_in, unsigned length_in, ParserType type_in, void*& values_out, unsigned iteration_in){
+void Parser::ImportAllValues(std::ifstream &file_in, unsigned length_in, ParserType type_in, void *&values_out, unsigned iteration_in)
+{
     unsigned size = length_in * iteration_in;
     switch (type_in)
     {
@@ -209,7 +227,8 @@ void Parser::ImportAllValues(std::ifstream& file_in, unsigned length_in, ParserT
     }
 }
 
-void Parser::ExportAllValues(std::ofstream& file_in, unsigned length_in, ParserType type_in, void* values_in, std::streampos& iterationPosition_in, unsigned iteration_in){
+void Parser::ExportAllValues(std::ofstream &file_in, unsigned length_in, ParserType type_in, void *values_in, std::streampos &iterationPosition_in, unsigned iteration_in)
+{
     std::streampos position = file_in.tellp();
     file_in.seekp(iterationPosition_in);
     file_in << iteration_in;
@@ -261,35 +280,38 @@ void Parser::ExportAllValues(std::ofstream& file_in, unsigned length_in, ParserT
     }
 }
 
-void Parser::ImportConfigurationBinary(std::ifstream& file_in, std::string& name_out, unsigned& length_out, ParserType& type_out, unsigned& iteration_out){
+void Parser::ImportConfigurationBinary(std::ifstream &file_in, std::string &name_out, unsigned &length_out, ParserType &type_out, unsigned &iteration_out)
+{
     unsigned type;
     unsigned size;
-    file_in.read((char*)(&size),sizeof(unsigned));
-    char* aux = new char[size];
-    file_in.read(aux,sizeof(char)*size);
-    file_in.read((char*)(&length_out),sizeof(unsigned));
-    file_in.read((char*)(&(type)),sizeof(unsigned));
-    file_in.read((char*)(&(iteration_out)),sizeof(unsigned));
-    name_out = std::string(aux,size);
+    file_in.read((char *)(&size), sizeof(unsigned));
+    char *aux = new char[size];
+    file_in.read(aux, sizeof(char) * size);
+    file_in.read((char *)(&length_out), sizeof(unsigned));
+    file_in.read((char *)(&(type)), sizeof(unsigned));
+    file_in.read((char *)(&(iteration_out)), sizeof(unsigned));
+    name_out = std::string(aux, size);
     type_out = (ParserType)type;
     delete[] aux;
 }
 
-void Parser::ExportConfigurationBinary(std::ofstream& file_in, std::string name_in, unsigned length_in, ParserType type_in, std::streampos& iterationPosition_out){
+void Parser::ExportConfigurationBinary(std::ofstream &file_in, std::string name_in, unsigned length_in, ParserType type_in, std::streampos &iterationPosition_out)
+{
     unsigned size = name_in.size();
     unsigned type = (unsigned)type_in;
     unsigned iteration = 0u;
-    file_in.write((char*)(&size),sizeof(unsigned));
-    file_in.write(name_in.c_str(),sizeof(char)*size);
-    file_in.write((char*)(&length_in),sizeof(unsigned));
-    file_in.write((char*)(&(type)),sizeof(unsigned));
+    file_in.write((char *)(&size), sizeof(unsigned));
+    file_in.write(name_in.c_str(), sizeof(char) * size);
+    file_in.write((char *)(&length_in), sizeof(unsigned));
+    file_in.write((char *)(&(type)), sizeof(unsigned));
     iterationPosition_out = file_in.tellp();
-    file_in.write((char*)(&iteration),sizeof(unsigned));
+    file_in.write((char *)(&iteration), sizeof(unsigned));
 }
 
-void Parser::ImportValuesBinary(std::ifstream& file_in, unsigned length_in, ParserType type_in, void*& values_out, unsigned iteration_in){
+void Parser::ImportValuesBinary(std::ifstream &file_in, unsigned length_in, ParserType type_in, void *&values_out, unsigned iteration_in)
+{
     std::streampos position = file_in.tellg();
-    file_in.seekg(position+(std::streamoff)(length_in*iteration_in));
+    file_in.seekg(position + (std::streamoff)(length_in * iteration_in));
     switch (type_in)
     {
     case ParserType::Char:
@@ -350,7 +372,8 @@ void Parser::ImportValuesBinary(std::ifstream& file_in, unsigned length_in, Pars
     file_in.seekg(position);
 }
 
-void Parser::ExportValuesBinary(std::ofstream& file_in, unsigned length_in, ParserType type_in, void* values_in, std::streampos& iterationPosition_in, unsigned iteration_in){
+void Parser::ExportValuesBinary(std::ofstream &file_in, unsigned length_in, ParserType type_in, void *values_in, std::streampos &iterationPosition_in, unsigned iteration_in)
+{
     std::streampos position = file_in.tellp();
     file_in.seekp(iterationPosition_in);
     file_in << iteration_in + 1u;
@@ -401,7 +424,8 @@ void Parser::ExportValuesBinary(std::ofstream& file_in, unsigned length_in, Pars
     }
 }
 
-void Parser::ImportAllValuesBinary(std::ifstream& file_in, unsigned length_in, ParserType type_in, void*& values_out, unsigned iteration_in){
+void Parser::ImportAllValuesBinary(std::ifstream &file_in, unsigned length_in, ParserType type_in, void *&values_out, unsigned iteration_in)
+{
     unsigned size = length_in * iteration_in;
     switch (type_in)
     {
@@ -462,10 +486,11 @@ void Parser::ImportAllValuesBinary(std::ifstream& file_in, unsigned length_in, P
     }
 }
 
-void Parser::ExportAllValuesBinary(std::ofstream& file_in, unsigned length_in, ParserType type_in, void* values_in, std::streampos& iterationPosition_in, unsigned iteration_in){
+void Parser::ExportAllValuesBinary(std::ofstream &file_in, unsigned length_in, ParserType type_in, void *values_in, std::streampos &iterationPosition_in, unsigned iteration_in)
+{
     std::streampos position = file_in.tellp();
     file_in.seekp(iterationPosition_in);
-    file_in.write((char*)&iteration_in,sizeof(unsigned));
+    file_in.write((char *)&iteration_in, sizeof(unsigned));
     file_in.seekp(position);
     unsigned size = length_in * iteration_in;
     switch (type_in)
@@ -514,134 +539,147 @@ void Parser::ExportAllValuesBinary(std::ofstream& file_in, unsigned length_in, P
     }
 }
 
-void Parser::DeleteValues(void* values_in, ParserType type_in){
+void Parser::DeleteValues(void *values_in, ParserType type_in)
+{
     switch (type_in)
     {
     case ParserType::Char:
-        delete[] ((char*)values_in);
+        delete[] ((char *)values_in);
         break;
     case ParserType::UChar:
-        delete[] ((unsigned char*)values_in);
+        delete[] ((unsigned char *)values_in);
         break;
     case ParserType::SInt:
-        delete[] ((short int*)values_in);
+        delete[] ((short int *)values_in);
         break;
     case ParserType::SUInt:
-        delete[] ((short unsigned int*)values_in);
+        delete[] ((short unsigned int *)values_in);
         break;
     case ParserType::Int:
-        delete[] ((int*)values_in);
+        delete[] ((int *)values_in);
         break;
     case ParserType::UInt:
-        delete[] ((unsigned int*)values_in);
+        delete[] ((unsigned int *)values_in);
         break;
     case ParserType::LInt:
-        delete[] ((long int*)values_in);
+        delete[] ((long int *)values_in);
         break;
     case ParserType::LUInt:
-        delete[] ((long unsigned int*)values_in);
+        delete[] ((long unsigned int *)values_in);
         break;
     case ParserType::LLInt:
-        delete[] ((long long int*)values_in);
+        delete[] ((long long int *)values_in);
         break;
     case ParserType::LLUInt:
-        delete[] ((long long unsigned int*)values_in);
+        delete[] ((long long unsigned int *)values_in);
         break;
     case ParserType::Float:
-        delete[] ((float*)values_in);
+        delete[] ((float *)values_in);
         break;
     case ParserType::Double:
-        delete[] ((double*)values_in);
+        delete[] ((double *)values_in);
         break;
     case ParserType::LDouble:
-        delete[] ((long double*)values_in);
+        delete[] ((long double *)values_in);
         break;
     default:
         break;
     }
 }
 
-void Parser::ConvertToBinary(std::string path_in, std::string path_out, std::string extension_in){
+void Parser::ConvertToBinary(std::string path_in, std::string path_out, std::string extension_in)
+{
     std::string name;
     unsigned length;
     ParserType type;
-    void* values;
+    void *values;
     unsigned sizeType;
     std::string name_in;
     std::string name_out;
-    for(const auto & entry : std::filesystem::directory_iterator(path_in)){
+    for (const auto &entry : std::filesystem::directory_iterator(path_in))
+    {
         name_in = path_in + entry.path().filename().string();
         name_out = path_out + entry.path().stem().string() + extension_in;
-        
+
         std::ifstream in(name_in);
         std::ofstream out(name_out, std::ios::binary | std::ios::trunc);
 
         std::streampos iterationPosition;
         unsigned iteration = 0u;
-        
-        ImportConfiguration(in,name,length,type,iteration);
-        ImportAllValues(in,length,type,values,iteration);
 
-        ExportConfigurationBinary(out,name,length,type,iterationPosition);
-        ExportAllValuesBinary(out,length,type,values,iterationPosition,iteration);
+        ImportConfiguration(in, name, length, type, iteration);
+        ImportAllValues(in, length, type, values, iteration);
 
-        DeleteValues(values,type);
+        ExportConfigurationBinary(out, name, length, type, iterationPosition);
+        ExportAllValuesBinary(out, length, type, values, iterationPosition, iteration);
+
+        DeleteValues(values, type);
 
         out.close();
         in.close();
     }
 }
 
-void Parser::ConvertToText(std::string path_in, std::string path_out, std::string extension_in){
+void Parser::ConvertToText(std::string path_in, std::string path_out, std::string extension_in)
+{
     std::string name;
     unsigned length;
     ParserType type;
-    void* values;
+    void *values;
     unsigned sizeType;
     std::string name_in;
     std::string name_out;
-    for(const auto & entry : std::filesystem::directory_iterator(path_in)){
+    for (const auto &entry : std::filesystem::directory_iterator(path_in))
+    {
         name_in = path_in + entry.path().filename().string();
         name_out = path_out + entry.path().stem().string() + extension_in;
-        
+
         std::ifstream in(name_in, std::ios::binary);
         std::ofstream out(name_out, std::ios::trunc);
 
         std::streampos iterationPosition;
         unsigned iteration = 0u;
-        
-        ImportConfigurationBinary(in,name,length,type,iteration);
-        ImportAllValuesBinary(in,length,type,values,iteration);
 
-        ExportConfiguration(out,name,length,type,iterationPosition);
-        ExportAllValues(out,length,type,values,iterationPosition,iteration);
+        ImportConfigurationBinary(in, name, length, type, iteration);
+        ImportAllValuesBinary(in, length, type, values, iteration);
 
-        DeleteValues(values,type);
+        ExportConfiguration(out, name, length, type, iterationPosition);
+        ExportAllValues(out, length, type, values, iterationPosition, iteration);
+
+        DeleteValues(values, type);
 
         out.close();
         in.close();
     }
 }
 
-unsigned Parser::OpenFileIn(std::string path_in, std::string name_in, std::string extension_in, std::ios::openmode mode_in, unsigned index_in){
+unsigned Parser::OpenFileIn(std::string path_in, std::string name_in, std::string extension_in, std::ios::openmode mode_in, unsigned index_in)
+{
     std::cout << "Trying to open file with name: " << name_in << "\n";
-    if(index_in == UINT_MAX_VALUE){
-        if(count_In >= length_In){
+    if (index_in == UINT_MAX_VALUE)
+    {
+        if (count_In >= length_In)
+        {
             std::cout << "Error: Too many files are open at the moment. Use old indexes to reopen in the same place or close all files.";
             return UINT_MAX_VALUE;
         }
         fileArray_In[count_In] = std::ifstream(path_in + name_in + extension_in, mode_in);
-        if(!fileArray_In[count_In].is_open()){
+        if (!fileArray_In[count_In].is_open())
+        {
             std::cout << "Error: Failed to open file.\n";
         }
         count_In++;
-        return count_In-1;
-    } else {
-        if(index_in >= length_In){
+        return count_In - 1;
+    }
+    else
+    {
+        if (index_in >= length_In)
+        {
             std::cout << "Error: Index is out of range.";
             return UINT_MAX_VALUE;
         }
-        if(fileArray_In[index_in].is_open()){
+        if (fileArray_In[index_in].is_open())
+        {
             fileArray_In[index_in].close();
         }
         fileArray_In[index_in] = std::ifstream(path_in + name_in + extension_in, mode_in);
@@ -649,25 +687,33 @@ unsigned Parser::OpenFileIn(std::string path_in, std::string name_in, std::strin
     }
 }
 
-unsigned Parser::OpenFileOut(std::string path_in, std::string name_in, std::string extension_in, std::ios::openmode mode_in, unsigned index_in){
+unsigned Parser::OpenFileOut(std::string path_in, std::string name_in, std::string extension_in, std::ios::openmode mode_in, unsigned index_in)
+{
     std::cout << "Trying to open file with name: " << name_in << "\n";
-    if(index_in == UINT_MAX_VALUE){
-        if(count_Out >= length_Out){
+    if (index_in == UINT_MAX_VALUE)
+    {
+        if (count_Out >= length_Out)
+        {
             std::cout << "Error: Too many files are open at the moment. Use old indexes to reopen in the same place or close all files.";
             return UINT_MAX_VALUE;
         }
         fileArray_Out[count_Out] = std::ofstream(path_in + name_in + extension_in, mode_in);
-        if(!fileArray_Out[count_Out].is_open()){
+        if (!fileArray_Out[count_Out].is_open())
+        {
             std::cout << "Error: Failed to open file.\n";
         }
         count_Out++;
-        return count_Out-1;
-    } else {
-        if(index_in >= length_Out){
+        return count_Out - 1;
+    }
+    else
+    {
+        if (index_in >= length_Out)
+        {
             std::cout << "Error: Index is out of range.";
             return UINT_MAX_VALUE;
         }
-        if(fileArray_Out[index_in].is_open()){
+        if (fileArray_Out[index_in].is_open())
+        {
             fileArray_Out[index_in].close();
         }
         fileArray_Out[index_in] = std::ofstream(path_in + name_in + extension_in, mode_in);
@@ -675,43 +721,59 @@ unsigned Parser::OpenFileOut(std::string path_in, std::string name_in, std::stri
     }
 }
 
-std::ifstream& Parser::GetStreamIn(unsigned index_in){
+std::ifstream &Parser::GetStreamIn(unsigned index_in)
+{
     return fileArray_In[index_in];
 }
-std::ofstream& Parser::GetStreamOut(unsigned index_in){
+std::ofstream &Parser::GetStreamOut(unsigned index_in)
+{
     return fileArray_Out[index_in];
 }
 
-void Parser::CloseFileIn(unsigned index_in){
+void Parser::CloseFileIn(unsigned index_in)
+{
     fileArray_In[index_in].close();
 }
-void Parser::CloseFileOut(unsigned index_in){
+void Parser::CloseFileOut(unsigned index_in)
+{
     fileArray_Out[index_in].close();
 }
 
-void Parser::CloseAllFileIn(){
-    for(unsigned i = 0u; i < length_In; i++){
-        if(fileArray_In[i].is_open()){
+void Parser::CloseAllFileIn()
+{
+    for (unsigned i = 0u; i < length_In; i++)
+    {
+        if (fileArray_In[i].is_open())
+        {
             fileArray_In[i].close();
         }
     }
     count_Out = 0u;
 }
-void Parser::CloseAllFileOut(){
-    for(unsigned i = 0u; i < length_Out; i++){
-        if(fileArray_Out[i].is_open()){
+void Parser::CloseAllFileOut()
+{
+    for (unsigned i = 0u; i < length_Out; i++)
+    {
+        if (fileArray_Out[i].is_open())
+        {
             fileArray_Out[i].close();
         }
     }
     count_Out = 0u;
 }
 
-
-Parser::~Parser(){
+Parser::~Parser()
+{
     CloseAllFileIn();
     CloseAllFileOut();
     length_In = 0u;
     length_Out = 0u;
-    delete[] fileArray_In;
-    delete[] fileArray_Out;
+    if (fileArray_In != NULL)
+    {
+        delete[] fileArray_In;
+    }
+    if (fileArray_Out != NULL)
+    {
+        delete[] fileArray_Out;
+    }
 }
